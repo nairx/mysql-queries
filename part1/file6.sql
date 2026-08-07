4360098f33280554cdb985a32ea872d7fe09e922
nairx <nairxprv@gmail.com>
2026-08-07 16:51:16 +0530

    updated

--- part1/file6.sql
+++ part1/file6.sql
@@ -11,3 +11,17 @@ select count(population) from city;
 select count(population) from city;
 
 select countrycode,min(population) from city group by countrycode;
+
+--display top 5 densly populated District/State
+select district, sum(population) as total_population from city group by district order by total_population desc limit 5;
+
+
+--display top 5 densly populated city district wise
+select district, max(population) as total_population from city group by district order by total_population desc limit 5;
+
+
+--display least 5  populated city district wise
+select district, min(population) as total_population from city group by district order by total_population desc limit 5;
+
+--Find the average population of countries.
+select countrycode, avg(population) from city group by countrycode;
