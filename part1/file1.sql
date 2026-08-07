269f184ff61d388476080d302f0d8171ce13023c
nairx <nairxprv@gmail.com>
2026-08-07 14:15:10 +0530

    updated

--- part1/file1.sql
+++ part1/file1.sql
@@ -5,3 +5,15 @@ show databases;
 system cls;  -- to clear the screen
 
 show tables; -- display all table names
+
+desc city;
+
+select * from city;
+
+select Name,CountryCode from city;
+
+select distinct CountryCode from city;
+
+select count(name) from city;
+
+select count(distinct CountryCode) from city;
