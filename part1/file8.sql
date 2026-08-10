b1aad4142150695c81616d41dced92e9b82a2b05
nairx <nairxprv@gmail.com>
2026-08-10 14:15:35 +0530

    updated

---
+++ part1/file8.sql
@@ -0,0 +1,28 @@
+elect ceil(avg(surfacearea))
+from
+    country;
+
+select
+    floor(avg(surfacearea))
+from
+    country;
+
+select
+    greatest (45, 3, 7, 3);
+
+select
+    least (45, 3, 7, 3);
+
+select
+    mod(5, 2);
+
+select
+    power(4, 2);
+
+select
+    round(4, 5);
+
+select
+    sqrt(9);
+
+    select truncate(35.678,2);
---
+++ part1/file9.sql
@@ -0,0 +1,15 @@
+select curdate(),name from city;
+
+select curdate(), curtime(), name from city limit 10;
+
+select now(), name from city limit 10
+
+select day(curdate());
+
+ select month(curdate());
+
+ select year(curdate());
+
+ select hour(curtime());
+
+ select minute(curtime());
