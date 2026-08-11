df789bdc1e18d915d81301f0b82c47c86056cd83
nairx <nairxprv@gmail.com>
2026-08-11 15:55:34 +0530

    updated

--- part3/file19.sql
+++ part3/file19.sql
@@ -1,8 +1,34 @@
 
-
-DELIMIER //
+DELIMIIER //
 CREATE PROCEDURE getUserById(IN userid int)
 BEGIN
 SELECT * FROM users where userid=userid;
 END //
-DELIMIER
+DELIMITER;
+
+
+DELIMITER //
+CREATE PROCEDURE getBalance(OUT balance int)
+BEGIN
+SELECT acc_balance
+into balance
+FROM users 
+where userid=1;
+END //
+DELIMITER;
+
+call getBalance(@balance)
+
+select @balance
+
+
+DELIMITER //
+CREATE PROCEDURE IncreaseValue(INOUT num int)
+BEGIN
+SET NUM = NUM + 10;
+END //
+DELIMITER;
+
+set @x = 10;
+call IncreaseValue(@x);
+select @x;
