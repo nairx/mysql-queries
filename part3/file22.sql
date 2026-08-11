e35e872855470ccff925141126d1ee51ed448ae9
nairx <nairxprv@gmail.com>
2026-08-11 17:00:18 +0530

    updated

--- part3/file22.sql
+++ part3/file22.sql
@@ -6,8 +6,13 @@ create table customer(
 );
 
 DELIMITER //
-CREATE PROCEDURE AddCustomer()
+CREATE PROCEDURE AddCustomer(IN p_id int, IN p_name varchar(100))
 BEGIN
-
+ DECLARE EXIT HANDLES FOR SQLEXCEPTION
+ BEGIN
+   SELECT 'An Error Occurred...';
+ END;
+ insert into customer
+ values(p_id,p_name);
 END //
 DELIMITER;
