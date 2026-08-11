79dcfcfd267fc0e0795da12be444bb1ed338fdac
nairx <nairxprv@gmail.com>
2026-08-11 16:49:34 +0530

    updated

---
+++ part3/file22.sql
@@ -0,0 +1,13 @@
+//Create Procedure to add new customer in emp table
+
+create table customer(
+    id int primary key,
+    name varchar(100)
+);
+
+DELIMITER //
+CREATE PROCEDURE AddCustomer()
+BEGIN
+
+END //
+DELIMITER;
